9226042159d4f0d9d56ce8a2f3e1e38c32936a90
QiZhongdd <1090018789@qq.com>
2020-09-23 23:44:08 +0800

    ss

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'mobx-react-lite/batchingForReactDom';
 import App from '@pages/App';
+import './assets/css/global.css'
 // import 'mobx-react-lite/batchingForReactDom';
 
 ReactDOM.render(<App />, document.getElementById('main'));
