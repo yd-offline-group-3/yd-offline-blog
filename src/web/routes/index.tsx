dd59804cb8fa593745fa1c91d81dcb21c7455acc
刘君杰 <liujunjie@ztebright.com>
2020-09-26 17:59:21 +0800

    添加Content路由

--- src/web/routes/index.tsx
+++ src/web/routes/index.tsx
@@ -10,6 +10,7 @@ const { lazy, Suspense } = React;
 
 const AboutUs = lazy(() => import(/* webpackChunkName:"AboutUs" */ '@pages/AboutUs'));
 const right = lazy(() => import(/* webpackChunkName:"Nav" */ '@components/RightComponet'));
+const Content = lazy(() => import(/* webpackChunkName:"Nav" */ '@pages/Content'));
 interface YDProps extends RouteProps {
   auth?: boolean;
 }
@@ -30,6 +31,11 @@ export const routes: YDProps[] = [
     path: '/AboutUs',
     exact: true,
     component: AboutUs,
+  },
+  {
+    path: '/content/:post',
+    exact: true,
+    component: Content,
   }
 ];
 
